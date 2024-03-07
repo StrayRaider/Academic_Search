45e712141a02cf2c1ebee316271436eaf54e4744
stray <stray@localhost.localdomain>
2024-03-07 14:21:06 +0300

    result system and connection created

--- academic_Search/polls/views.py
+++ academic_Search/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import searcheds
 from textblob import TextBlob
+from django.http import JsonResponse
 
 def index(request):
     #if request.method == 'POST':
@@ -26,5 +27,19 @@ def show_results(request):
     if 'param1' in request.GET:
         corrected_query = TextBlob(request.GET['param1']).correct()
         param1 = request.GET['param1']
-        return HttpResponse(f"corrected : {corrected_query}")
-    return render(request,'results.html')
+
+        # do searchs
+
+        #return HttpResponse(f"corrected : {corrected_query}")
+    card_data_list = [
+        {'title': 'Card 1', 'description': 'This is the first card.', 'url': '/link-to-card-1/'},
+        {'title': 'Card 2', 'description': 'This is the second card.', 'url': '/link-to-card-2/'},
+        # Add more cards as needed
+    ]
+
+    return render(request, 'results.html', {'cards_data': card_data_list})
+    #return render(request,'results.html',{"param1": corrected_query})
+
+def animation(request):
+    scraped_data = ["data1", "data2", "data3"]
+    return JsonResponse({"data": scraped_data})
